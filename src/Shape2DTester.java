17660d15ff676a95324d8d8a28328ed066676171
naziipao <nazifahmedrifat@gmail.com>
2024-12-04 21:29:17 +0600

    Commit

--- src/Shape2DTester.java
+++ src/Shape2DTester.java
@@ -4,15 +4,15 @@ public class Shape2DTester {
         System.out.println("---------1----------");
         sq.area();
         System.out.println("---------2----------");
-        Shape2D rectangle = new Shape2D(5,6);
+        Shape2D rectangle = new Shape2D(5, 6);
         System.out.println("---------3----------");
         rectangle.area();
         System.out.println("---------4----------");
-        Shape2D tri1 = new Shape2D(5,6,"Triangle");
+        Shape2D tri1 = new Shape2D(5, 6, "Triangle");
         System.out.println("---------5----------");
         tri1.area();
         System.out.println("---------6----------");
-        Shape2D tri2 = new Shape2D(5,6,7);
+        Shape2D tri2 = new Shape2D(5, 6, 7);
         System.out.println("---------7----------");
         tri2.area();
         System.out.println("---------8----------");
